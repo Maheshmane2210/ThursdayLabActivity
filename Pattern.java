a7377cc7a8884e11b12a9fa7ac4d30818f944541
Maheshmane2210 <118435449+Maheshmane2210@users.noreply.github.com>
2022-11-22 10:20:03 +0530

    Add files via upload

---
+++ Pattern.java
@@ -0,0 +1,16 @@
+package question2;
+
+	public class pattern {
+
+		public static void main(String[] args) {
+			int number=1;
+			for(int i=0;i<=4;i++)
+			{
+				for(int j=0;j<=i;j++)
+				{
+					System.out.print(number++ +" ");
+				}
+				System.out.println();
+			}
+		}
+	}
