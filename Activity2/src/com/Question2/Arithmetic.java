9e8014d7fecf6d0ef60d2001c8f58a0d55dc369e
Mahesh <Mahesh@LAPTOP-LKJQDASP>
2022-11-29 12:13:35 +0530

    hi activity2

---
+++ Activity2/src/com/Question2/Arithmetic.java
@@ -0,0 +1,19 @@
+package com.Question2;
+
+public class Arithmetic {
+	
+		int add(int a,int b)
+		{
+			int sum=a+b;
+			return(sum);
+		}
+
+	}
+	class Adder extends Arithmetic
+	{
+		public int addition(int a, int b)
+		{
+			return add(a,b);
+		}
+	}
+	
---
+++ Activity2/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Mahesh
+ *
+ */
+module Activity2 {
+}
